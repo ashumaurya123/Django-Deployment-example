c810473dc78c230bca12707e7d38aa29dbae644e
Ashutosh <ashutoshmaurya1998@gmail.com>
2022-09-15 22:08:00 +0530

    first

---
+++ django_level_4/learning_templates/basic_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from basic_app import views
+
+
+def index(request):
+    return render(request, 'basic_app/index.html')
+
+
+def other(request):
+    return render(request, 'basic_app/other.html')
+
+
+def relative(request):
+    return render(request, 'basic_app/relative_url_templates.html')
+
